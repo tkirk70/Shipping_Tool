0611f9b0a5dfafc5a473b9fdc963c9d7803de507
TDS <tds@tcg3pl.com>
2024-02-27 15:54:19 -0500

    Adding multiplier button.

--- Shipping_Tool.py
+++ Shipping_Tool.py
@@ -64,10 +64,10 @@ print(f'The distance from TCG is: {distance:,.0f} miles.')
 # add multiplier button for TCG or Customer cost
 st.button("Cost", type="primary")
 if st.button('Upcharge'):
-    st.write(":green[20% Surcharge for Customer]")
+    st.write(":violet[20% Surcharge for Customer]")
     multiplier = 1.2
 else:
-    st.write(":green[TCG Cost]")
+    st.write(":violet[TCG Cost]")
     multiplier = 1
 
 # from copilot
