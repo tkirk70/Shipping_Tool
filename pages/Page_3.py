99b4213e27d25ac2a3b364a5b4c3fdc2d55d7ba4
TDS <tds@tcg3pl.com>
2024-02-28 13:50:21 -0500

    Adding third page.

---
+++ pages/Page_3.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Using set_page_config()')
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
