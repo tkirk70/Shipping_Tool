0d521506c0e64f1aad98a26b184af17bb94b108e
TDS <tds@tcg3pl.com>
2024-03-07 09:21:42 -0500

    Adding columns 4 and 5

--- Shipping_Tool.py
+++ Shipping_Tool.py
@@ -30,7 +30,7 @@ col4, col5 = st.columns(2)
 with col4:
     zip_code = st.text_input("What is the zip?", "43123")
  with col5:
-     weight = st.number_input("What is the weight?", 1)
+    weight = st.number_input("What is the weight?", 1)
         
 col1, col2, col3 = st.columns(3)
 
